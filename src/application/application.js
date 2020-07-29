c78235a86fdc53175a763cc075648d77227f82f4
kathiravanltts <62378427+kathiravanltts@users.noreply.github.com>
2020-07-29 14:06:55 +0530

    This pull request for 'Channelbar','MastHead' and 'NumberInput' feature . (#28)
    * Channelbar,MastHead,NumberInput update
    
    * package json update
    
    * NumberInput Issue fixed
    
    * Allow only number 0 to 9
    
    * package.json update revert
    
    * header position modified
    
    * filename modified

--- src/application/application.js
+++ src/application/application.js
@@ -77,8 +77,8 @@ export default class Application extends Lightning.Component {
 
   //This method validated the input value to channel tune.
   numberInput(evt) {
-    let keyValue = parseInt(evt.key)
-    if (keyValue >= 0 && keyValue <= 9) {
+    let keyValue = parseInt(evt.key);
+    if (keyValue >= 0 && keyValue <= 9 ) {
       let item = {
         label: 'NumberInput',
         type: 'Notification',
